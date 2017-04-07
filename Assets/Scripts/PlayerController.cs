1b2d19f93c2ef549a15ea30b5cc750b90ed8991c
shieldgenerator7 <shieldgenerator7@yahoo.com>
2017-04-06 20:32:42 -0400

    Added PlayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;//the speed pacman can travel
+
+    Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            rb2d.velocity = Vector2.left * speed;
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            rb2d.velocity = Vector2.right * speed;
+        }
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            rb2d.velocity = Vector2.down * speed;
+        }
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            rb2d.velocity = Vector2.up * speed;
+        }
+    }
+}
