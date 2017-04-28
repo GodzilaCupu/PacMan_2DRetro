c83249202b60541c205ade9442e7c3ccc6efba84
shieldgenerator7 <shieldgenerator7@yahoo.com>
2017-04-27 20:58:13 -0400

    GameOver screen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
@@ -9,6 +10,9 @@ public class GameManager : MonoBehaviour {
         PLAY, PACMAN_DYING, PACMAN_DEAD, GAME_OVER, GAME_WON
     };
     public GameState gameState = GameState.PLAY;
+
+    public Image gameWonScreen;
+    public Image gameOverScreen;
 
     public GameObject pacman;
     public AnimationClip pacmanDeathAnimation;
@@ -27,7 +31,9 @@ public class GameManager : MonoBehaviour {
         {
             Destroy(gameObject);
         }
-	}
+        gameOverScreen.enabled = false;
+        gameWonScreen.enabled = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -47,14 +53,25 @@ public class GameManager : MonoBehaviour {
                     gameState = GameState.PLAY;
                     pacman.SetActive(true);
                     PlayerController playerController = pacman.GetComponent<PlayerController>();
-                    playerController.setAlive(true);
                     playerController.setLivesLeft(playerController.livesLeft - 1);
+                    if (playerController.livesLeft >= 0)
+                    {
+                        playerController.setAlive(true);
+                    }
+                    else
+                    {
+                        gameState = GameState.GAME_OVER;
+                    }
                     pacman.transform.position = Vector2.zero;
                     foreach (GameObject ghost in ghosts)
                     {
                         ghost.GetComponent<GhostController>().reset();
                     }
                 }
+                break;
+            case GameState.GAME_OVER:
+                gameOverScreen.enabled = true;
+                gameWonScreen.enabled = false;
                 break;
         }
 	}
